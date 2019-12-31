afdb662b5acb08da2461f583b243edb0f4568e72
vuong.van <vuong.van@ascendcorp.com>
2019-12-31 16:18:23 +0700

    Remove comments

--- src/main/java/AutomationSupport/AutomationSupport.java
+++ src/main/java/AutomationSupport/AutomationSupport.java
@@ -12,13 +12,11 @@ import CSV.CSVWriter;
 public class AutomationSupport {
     public static void main(String[] args) throws IOException {
         String filePath = "./sample.csv";
-        String outputFilePath = "C:\\Users\\VVV\\o2o-api-automation\\api\\testcases\\test.csv";
         String apiParentFilePath = "C:\\Users\\VVV\\o2o-api-automation\\api";
 
         TestSuite testSuite = new TestSuite();
         Keywords keywords = new Keywords();
         testSuite.generatingTestSuiteFromInputFile(filePath, apiParentFilePath);
         keywords.generatingKeywordsFileFromInputFile(filePath,apiParentFilePath);
-//        testSuite.generateTestSuiteFileContent(filePath, outputFilePath);
     }
 }
