7fe2e3c7c1a85e10fbeb5672be3fb6ca344e12b8
vuong.van <vuong.van@ascendcorp.com>
2019-12-31 16:04:46 +0700

    Remove comments

--- src/main/java/AutomationSupport/AutomationSupport.java
+++ src/main/java/AutomationSupport/AutomationSupport.java
@@ -14,16 +14,6 @@ public class AutomationSupport {
         String filePath = "./sample.csv";
         String outputFilePath = "C:\\Users\\VVV\\o2o-api-automation\\api\\testcases\\test.csv";
         String apiParentFilePath = "C:\\Users\\VVV\\o2o-api-automation\\api";
-//        CSVReader csvReader = new CSVReader();
-//        List<String> nameList = Arrays.asList();
-//        nameList = csvReader.readCSVFileByHeader("./users-with-header.csv", "Name");
-//        System.out.println(nameList);
-
-//        CSVWriter csvWriter = new CSVWriter();
-//        csvWriter.writeToCsv("./sample.csv", "VVV");
-//        String[] messages = new String[]{"VVV1", "VVV2"};
-//        csvWriter.apprendToCsv("./sample.csv", messages);
-
 
         TestSuite testSuite = new TestSuite();
         Keywords keywords = new Keywords();
