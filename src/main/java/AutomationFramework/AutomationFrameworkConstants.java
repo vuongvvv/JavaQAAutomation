42d23b035f93833f4b73e53ae9e377862990c19a
Vuong Van <vuong.van@ascendcorp.com>
2020-01-08 17:40:18 +0700

    Update File Path delimiter

--- src/main/java/AutomationFramework/AutomationFrameworkConstants.java
+++ src/main/java/AutomationFramework/AutomationFrameworkConstants.java
@@ -1,7 +1,9 @@
 package AutomationFramework;
 
+import java.io.File;
+
 public class AutomationFrameworkConstants {
-    public static final String PATH_DELIMITER = "\\";
+    public static final String PATH_DELIMITER = File.separator;
     public static final String NEW_LINE = "\n";
     public static final String SPACES_FORMAT = "    ";
 }
